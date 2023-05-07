bde3eb52c9cc7dbf6458776cf92562f02baaf855
Nitant Makwana <nmakwana496@gmail.com>
2023-05-07 23:46:01 +0530

    Add files via upload

---
+++ Button.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { TouchableHighlight , StyleSheet, Text } from 'react-native'
+
+
+
+function Button({label , type , handlePress , icon}) {
+
+    const getTextColor =
+        type === "operator" ? "green" : type === "clear" ? "red" : "#fff";
+    
+    const getButtonColor =
+        type === "equal"
+            ? {backgroundColor: "green"}
+            :{backgroundColor: "#171717"}
+
+  return (
+    <TouchableHighlight
+     underlayColor={type == 'equal' ? "#04ac04" : "#444444"}
+     style = {[styles.button, getButtonColor]}
+     onPress = {() => handlePress(label)}
+    >
+        {icon ? (
+        icon
+      ) : (
+        <Text style={{ fontSize: 26, color: getTextColor, fontWeight: "bold" }}>
+          {label}
+        </Text>
+      )}
+
+    </TouchableHighlight>
+  )
+}
+
+export default Button
+
+
+const styles = StyleSheet.create({
+    button: {
+        borderRadius : 50,
+        backgroundColor: "#171717",
+        width: 80,
+        height: 80,
+        alignItems: "center",
+        justifyContent: "center"
+    }
+})
